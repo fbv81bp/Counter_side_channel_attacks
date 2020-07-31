12125a04b141712db7b655b32a138e7909885853
Fekete Balázs <fbv81bp@outlook.hu>
2020-07-31 09:41:57 +0200

    Added more comments

--- masked_AND_by_me/and.py
+++ masked_AND_by_me/and.py
@@ -2,22 +2,27 @@
 # for every i this algorithm calculates:
 # Ci = [XOR(k!=i: Ak) AND XOR(j!=i: Bj)] XOR (Ai AND Bi)
 
-# This implementation uses shift registers and calculates 1 single output bit at a clock cycle.
-
 # The generic parameters enable arbitrary wide words to be AND-ed which consist of arbitrary odd
 # number masked shares. The algorithm does not work with an even number of shares due to its math
 # properties!
 
 def masked_AND(am, bm):
+    #calculate AND between masked values
     cm = [am[i] & bm[i] for i in range(r)]
+    #calculating correction terms
     for i in range(r):
         suma = 0
         sumb = 0
+        #calculating parities of all corresponding Aj and Bj bits...
         for j in range(r):
+            #...corresponding means all but the Ai and Bi bits
             if i==j:
                 continue
+            #parities
             suma ^= am[j]
             sumb ^= bm[j]
+        #addig correction term to the particular output share
+        #which is the "AND" between the parities
         cm[i] ^= suma & sumb
     return cm
 
@@ -32,17 +37,17 @@ print("AND:    ", hex(a & b))
 # preparing odd pieces of random numbers for masking
 # the algorithm doesn't work with an even number of shares!
 
-r = 5 #6
+r = 5 #6 #uncomment to test even number of shares
 ra =[0x67978544,
      0xa6f328ab,
      0x68979586,
      0xc9876d3e]
-     #0xa7656b36]
+     #0xa7656b36] #uncomment to test even number of shares
 rb =[0x6abdc744,
      0x895728ab,
      0xfe45c8d6,
      0xc9876d3e]
-     #0x86f675ed]
+     #0x86f675ed] #uncomment to test even number of shares
 
 # the sum of the masks has to be zero at first...*1
 rasum = 0
